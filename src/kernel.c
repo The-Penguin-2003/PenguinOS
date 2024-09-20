acd13a71c1b4f3a7276991e37aab2d779a1ed9fd
The Penguin <halstonr2003@gmail.com>
2024-09-20 15:36:08 -0700

    Hello, OSDev World!

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,20 @@
 /* PenguinOS Kernel */
-#include <types.h>
+#include <vga.h>
 
 int32_t kernel_main()
 {
+	init_vga();
+	set_text_color(14, 0);
+	puts("Welcome to PenguinOS!\r\n");
+	set_text_color(15, 0);
+
+	for (int32_t i = 0; i < 16; i++)
+	{
+		set_text_color(i, i);
+		putc(' ');
+	}
+	puts("\r\n");
+
 	for (;;);
 
 	return 0;	// Should never get here
