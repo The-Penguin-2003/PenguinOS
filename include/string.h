9b1e257905a9425eea2e47d0e66b67170f3899bb
The Penguin <halstonr2003@gmail.com>
2024-09-20 14:56:15 -0700

    Add more string manipulation functions

--- include/string.h
+++ include/string.h
@@ -5,4 +5,8 @@
 
 int32_t strlen(int8_t* str);
 
+extern uint8_t* memcpy(uint8_t* dst, const uint8_t* src, int32_t cnt);
+extern uint8_t* memset(uint8_t* dst, uint8_t val, int32_t cnt);
+extern uint16_t* memsetw(uint16_t* dst, uint16_t val, int32_t cnt);
+
 #endif /* __STRING_H__ */
--- src/string.c
+++ src/string.c
@@ -13,3 +13,33 @@ int32_t strlen(int8_t* str)
 
 	return len;
 }
+
+uint8_t* memcpy(uint8_t* dst, const uint8_t* src, int32_t cnt)
+{
+	int32_t i = 0;
+
+	for (; i < cnt; ++i)
+		dst[i] = src[i];
+
+	return dst;
+}
+
+uint8_t* memset(uint8_t* dst, uint8_t val, int32_t cnt)
+{
+	int32_t i = 0;
+
+	for (; i < cnt; ++i)
+		dst[i] = val;
+
+	return dst;
+}
+
+uint16_t* memsetw(uint16_t* dst, uint16_t val, int32_t cnt)
+{
+	int32_t i = 0;
+
+	for (; i < cnt; ++i)
+		dst[i] = val;
+
+	return dst;
+}
