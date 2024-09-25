0f66c52fe82450e3a8eda9086a17ac51e27e8308
The Penguin <halstonr2003@gmail.com>
2024-09-25 13:24:11 -0700

    Add a couple new types for later use

--- include/types.h
+++ include/types.h
@@ -1,6 +1,8 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
+#define NULL ((void*)0)
+
 typedef char int8_t;
 typedef short int16_t;
 typedef int int32_t;
@@ -8,5 +10,7 @@ typedef int int32_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+
+typedef int size_t;
 
 #endif /* __TYPES_H__ */
